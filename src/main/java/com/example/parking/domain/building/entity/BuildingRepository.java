5d1a8644fa98354b0a7f96ef75e7a2511e1022b5
sangwon <swchoi1997@naver.com>
2022-10-23 21:09:17 +0900

    fix : UUID -> Long | JPQGL문법 오류 수정

--- src/main/java/com/example/parking/domain/building/entity/BuildingRepository.java
+++ src/main/java/com/example/parking/domain/building/entity/BuildingRepository.java
@@ -4,20 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.UUID;
+정public interface BuildingRepository extends JpaRepository<Building, Long> {
+    boolean existsByBuildingAddressZipCode(final String zipCode);
 
-public interface BuildingRepository extends JpaRepository<Building, UUID> {
-    boolean existsByBuildingZipCode(String zipCode);
-
-    boolean existsByBuildingName(String name);
+    boolean existsByName(final String name);
 
     @Query(value = "select building" +
             " from Building building" +
-            " where building.name = :name and building.building_zipCode = :building_zipCode")
+            " where building.name = :name and building.buildingAddress.zipCode = :buildingZipCode")
     //블로그에 적을 것
     // Cannot resolve
 //    해당 에러는 cirtical한 에러가 아니다. 그저 Member라는 Table의 구조를 파악할수 없다는 경고로 해결하지 않아도 정상적으로 작동한다.
 //    이때 가장 간단한 해결 방법은 해당 에러를 잡지 않도록 하는 방법이다
     //https://an-thropology.tistory.com/37
-    Building findByNameAndAddress(@Param("name") String name, @Param("building_zipCode") String building_zipCode);
+    Building findByNameAndAddress(@Param("name") final String name, @Param("buildingZipCode") final String building_zipCode);
 }
